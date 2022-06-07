044202850b40473db1fa1f8ebfd361814ab919e4
Daniel Farnand <dfarnand@gmail.com>
2022-06-06 23:00:53 -0400

    feat: major code cleanup

--- src/functions.R
+++ src/functions.R
@@ -1,140 +1,84 @@
-library(klaR)
+library(e1071)
 library(entropy)
 library(psych)
 
-NBTest <-
-  function(dataset,
-           R = 10,
-           K = 10,
-           clname = "Classes",
-           verbose = F) {
-    #' NB Test
-    #'
-    #' Function does a cross-validation test on Naive Bayes analysis for the dataset
-    #' Pass it the name of the Class column as clname
-    corRate <- 0
-    
-    # tryCatch( {
-    # Makes the formula using the columns
-    cols <- dimnames(dataset)[[2]]
-    len = length(cols)
-    paste(cols[1:(len - 1)], collapse = "+") %>%
-      paste(clname, "~", ., sep = "") %>%
-      as.formula() -> expr
-    
-    # Makes sure the classes column is a factor
-    dataset[, clname] <- as.factor(dataset[, clname])
-    
-    nObs <- nrow(dataset)
-    for (i in 1:R) {
-      ind <- sample(rep(1:K, each = nObs / K), replace = F)
-      # Creates a random order of numbers 1-K (equal numbers of each)
-      
-      corSum <- 0 #Running total of correct classifications
-      for (j in 1:K) {
-        # Leaving out 10% of the data randomly and then using the rest to predict
-        # its points, and checking how many are correctly predicted.
-        # tryCatch ( {  # Because the first function fails when it finds a zero variance
-        tempResult <- NaiveBayes(expr, data = dataset[ind != j,])
-        preds <- predict(tempResult, dataset[ind == j,])$class
-        # },
-        # error=function(err) {
-        #   tempResult <- naiveBayes(expr, data=dataset[ind!=j,])
-        #   preds <- predict(tempResult,dataset[ind==j,])
-        # })
-        
-        corCount <- sum(preds == dataset[, clname][ind == j])
-        corSum <- corSum + corCount
-      }
-      corRate <- corRate + (corSum / nObs)
-    }
-    return(corRate / R)
-    # },
-    # error=function(err) {
-    #   if (verbose) print(err)
-    #   return(NA)
-    # })
-    
-    
-  }
-
-
-NBClass <- function(dataset,
-                    clname = "Classes",
-                    verbose = F) {
-  #' NB Class
+NBTest <- function(dataset,
+                   n_trials = 10,
+                   n_cv_groups = 10,
+                   clname = "Classes",
+                   verbose = F) {
+  #' NB Test
   #'
-  #' Function to analyze classification for a single dataset
-  tryCatch({
-    cols <- dimnames(dataset)[[2]]
-    len = length(cols)
-    paste(cols[1:(len - 1)], collapse = "+") %>%
-      paste(clname, "~", ., sep = "") %>%
-      as.formula() -> expr
+  #' Function does a cross-validation test on Naive Bayes analysis for the dataset
+  #' Pass it the name of the Class column as clname
+  #' @param dataset Dataframe to test
+  #' @param n_trials number of times the test will be run; results are averaged across all trials. (default 10)
+  #' @param n_cv_groups how many groups used to cross validate? (default 10)
+  #' @param clname name of the column with class labels
+  #' @return Returns a numeric accuracy rate
+  
+  # Makes the formula using the columns
+  expr = as.formula(paste0(clname, "~ ."))
+  
+  n_obs <- nrow(dataset)
+  cor_rate <- 0
+  for (i in 1:n_trials) {
+    ind <- sample(rep(1:n_cv_groups, each = ceiling(n_obs / n_cv_groups)),
+                  size = n_obs,
+                  replace = F)
+    # Creates a random order of numbers 1-K (equal numbers of each)
     
-    return(NaiveBayes(expr, data = dataset))
-  },
-  error = function(err) {
-    if (verbose)
-      print(err)
-    return(NA)
-  })
+    cor_sum <- 0 #Running total of correct classifications
+    for (j in 1:n_cv_groups) {
+      # Leaving out 10% of the data randomly and then using the rest to predict
+      # its points, and checking how many are correctly predicted.
+      temp_result <- naiveBayes(expr, data = dataset[ind != j, ])
+      preds <- predict(temp_result, dataset[ind == j, ])
+      
+      cor_count <- sum(unlist(dataset[ind == j, clname]) == preds)
+      cor_sum <- cor_sum + cor_count
+    }
+    cor_rate <- cor_rate + (cor_sum / n_obs)
+  }
+  return(cor_rate / n_trials)
 }
 
 
-PCACompare <- function(dataset, verbose = F) {
+PCACompare <- function(dataset,
+                       dataset_name = NA,
+                       verbose = F) {
   #' PCA Compare
   #'
   #' Function that will take the dataset its given, do NB CV on both the original
   #' and the principle components version, and then return a vector containing the
   #' resulting scores, as well as other summary info about the dataset.
   #' The classes column needs to be the last one!
-  # tryCatch( {
   len = length(dimnames(dataset)[[2]])
   names(dataset)[len] <- "Cl"
   datapca <- as.data.frame(princomp(dataset[1:(len - 1)])$scores)
   datapca$Cl <- dataset$Cl
-  # },
-  # error=function(err){
-  #   if (verbose) print(err)
-  #   return(NA)
-  # })
   
-  # tryCatch( {
-  # results <- vector(mode = "list", length = 8)
-  # names(results) = c("Orig",
-  #                    "PCA",
-  #                    "Diff",
-  #                    "NFeatures",
-  #                    "NGroups",
-  #                    "NObs",
-  #                    "Entropy",
-  #                    "KMO")
+  # Check to make sure the class column is a factor
+  stopifnot(is.factor(dataset$Cl))
+  stopifnot(is.factor(datapca$Cl))
   
-  #  results$Name <- dsname
+  orig_res = NBTest(dataset, clname = "Cl", verbose = verbose)
+  pca_res = NBTest(datapca, clname = "Cl", verbose = verbose)
   results <- c(
-    "Orig" = NBTest(dataset, clname = "Cl", verbose = verbose),
-    "PCA" = NBTest(datapca, clname = "Cl", verbose = verbose),
-    "Diff" = results$PCA - results$Orig,
+    "Orig" = orig_res,
+    "PCA" = pca_res,
+    "Diff" = pca_res - orig_res,
     "NFeatures" = len - 1,
     "NGroups" = length(unique(dataset$Cl)),
     "NObs" = length(dimnames(dataset)[[1]]),
     "Entropy" = entropy.plugin(eigen(cor(dataset[, -len]))$values / dim(dataset[, -len])[2]),
     "KMO" = KMO(dataset[, -len])$MSA
   )
-  # results$Orig <- NBTest(dataset, clname = "Cl", verbose = verbose)
-  # results$PCA <- NBTest(datapca, clname = "Cl", verbose = verbose)
-  # results$Diff <- results$PCA - results$Orig
-  # results$NFeatures <- len - 1
-  # results$NGroups <- length(unique(dataset$Cl))
-  # results$NObs <- length(dimnames(dataset)[[1]])
-  # results$Entropy <-
-  #   entropy.plugin(eigen(cor(dataset[, -len]))$values / dim(dataset[, -len])[2])
-  # results$KMO <- KMO(dataset[, -len])$MSA
+  # Add an extra column to name dataset
+  if (!is.na(dataset_name)) {
+    results["dataset"] = dataset_name
+  }
+  
+  
   return(results)
-  # },
-  # error=function(err){
-  #   if (verbose) print(err)
-  #   return(NA)
-  # })
 }
--- src/pca_nb.R
+++ src/pca_nb.R
@@ -1,279 +1,238 @@
-#############################################################################
-## Daniel Farnand                                                          ##
-## Testing Naive Bayes Classification with and without Principle Component ##
-## Analysis on Fundamental Clustering Problems Suite (FCPS)                ##
-#############################################################################
+## Daniel Farnand
+## Testing Naive Bayes Classification with and without Principle Component
+## Analysis on Fundamental Clustering Problems Suite (FCPS)
 
-library(here)
-source(here("src", "functions.R"))
+# Set this to true run only on datasets that are built into the packages
+BUILTIN_ONLY = FALSE
 
-# require(klaR)
 library(tidyverse)
-# require(foreign)
-# require(e1071)
-# require(mlbench)
-# require(entropy)
-# require(psych)
-# require(rgl)
+library(here)
 
-# #################################################################################
-# ## Function to Simplify adding a row to a dataframe and updating the rownames. ##
-# #################################################################################
-# 
-# dfaddrow <- function(datafr, newrow, rowname) {
-#   temprows <- rownames(datafr)
-#   datafr <- rbind(datafr,newrow)
-# 
-#   rownames(datafr) <- c(temprows,rowname)
-# 
-#   return(datafr)
-# }
+source(here("src/functions.R"))
 
-
-################################################################################
-
-print("Getting started")
 set.seed(2011)
-
-#######################################################
-## Building a Dataframe of Different Dataset Results ##
-#######################################################
-
-## Importing Data
-files <- c("Hepta",
-           "Lsun", 
-           "Tetra", 
-           # "Chainlink", 
-           "Atom", 
-           "EngyTime", 
-           "Target",
-           "TwoDiamonds", 
-           "WingNut", 
-           "GolfBall")
-lrn_filenames = here("data", "FCPS", "01FCPSdata", str_c(files, ".lrn"))
-cls_filenames = here("data", "FCPS", "01FCPSdata", str_c(files, ".cls"))
-
-# fcpsdata <- vector(mode="list", length=length(files))
-fcps_data <-
-  map(
-    lrn_filenames,
-    read_tsv,
-    comment = "%",
-    col_names = FALSE,
-    col_select = -X1
-  ) %>% setNames(files)
-
-fcps_cls_data <-
-  map(
-    cls_filenames,
-    read_tsv,
-    comment = "%",
-    col_names = c("X1", "Classes"),
-    # col_types = "if",
-    col_select = -X1
-  ) %>% setNames(files)
-
-fcps <- map2(fcps_data, fcps_cls_data, bind_cols)
-PCACompare(fcps[[3]])
+results_list = list()
 
 
-  # map(files, ~read_csv(paste("FCPS/01FCPSdata/",.,".lrn",sep=""),
-  #                                 header=T,row.names=1,sep="\t",comment.char="%"))
-names(fcpsdata) <- files
-for(d in seq_along(fcpsdata)) {
-  fcpsdata[[d]]$Classes <- factor(unlist(read.table(paste("FCPS/01FCPSdata/"
-                                                         ,files[d],".cls",sep=""),
-                                                    header=F,row.names=1,sep="\t",
-                                                    comment.char="%")))
+## FCPS Datasets* ####
+
+if (!BUILTIN_ONLY) {
+  files <- c("Hepta",
+             "Lsun",
+             "Tetra",
+             "Atom",
+             "EngyTime",
+             "Target",
+             "TwoDiamonds",
+             "WingNut")
+  lrn_filenames = here("data/FCPS/01FCPSdata", str_c(files, ".lrn"))
+  cls_filenames = here("data/FCPS/01FCPSdata", str_c(files, ".cls"))
+  
+  # Loading the data and labels for each dataset separately, then combining
+  fcps_data <-
+    map(
+      lrn_filenames,
+      read_tsv,
+      comment = "%",
+      col_names = FALSE,
+      col_select = -X1
+    ) %>% setNames(files)
+  
+  fcps_cls_data <-
+    map(
+      cls_filenames,
+      read_tsv,
+      comment = "%",
+      col_names = c("X1", "Classes"),
+      col_types = "if",
+      col_select = -X1
+    ) %>% setNames(files)
+  
+  fcps <- map2(fcps_data, fcps_cls_data, bind_cols)
+  
+  res_fcps <- map2(fcps, files, ~PCACompare(.x, dataset_name = .y))
+  results_list <- append(results_list, res_fcps)
 }
 
 
-resultsDF <- data.frame()
+## Crabs Data ####
 
-for (ds in seq_along(fcpsdata)) {
-  resultsDF <- df.addrow(resultsDF, PCA.Compare(fcpsdata[[ds]]), files[ds])}
+data(crabs, package="MASS")
 
+crabs <- crabs %>%
+  mutate(Classes = factor(2 * (crabs$sex == "F") + (crabs$sp == "O"))) %>%
+  select(FL, RW, CL, CW, BD, Classes)
 
-
-
-################
-## Crabs Data ##
-################
-
-crabs <- read.dta("crabs.dta")
-crabs$Classes <- factor(2*(crabs$sex=="Female")+(crabs$species=="Orange"))
-
-resultsDF <- df.addrow(resultsDF,PCA.Compare(crabs[,4:9]),"Crabs")
+results_list <-
+  append(results_list, list(PCACompare(crabs, dataset_name = "Crabs")))
 
 
-###############
-## Iris Data ##
-###############
+## Iris Data #####
 
 data(iris)
-##irispca <- as.data.frame(princomp(iris[1:4])$scores)
-##irispca$Species <- iris$Species
-
-## NB.Test(iris, clname="Species")
-## NB.Test(irispca, clname="Species")
-
-resultsDF <- df.addrow(resultsDF,PCA.Compare(iris),"Iris")
+results_list <-
+  append(results_list, list(PCACompare(iris, dataset_name = "Iris")))
 
 
-###############
-## Wine Data ##
-###############
-wine <- read.table("wine/wine.data",sep=",")
-wine[,15] <- as.factor(wine[,1])
-wine <- wine[-1]
+## Wine Data* ####
 
-resultsDF <- df.addrow(resultsDF,PCA.Compare(wine),"Wine")
+if (!BUILTIN_ONLY) {
+  wine <- read_csv(here("data/wine.data"), col_names = FALSE) %>%
+    select(X2:X14, Classes = X1) %>%
+    mutate(Classes = as.factor(Classes))
 
-
-###############
-## Spam Data ##
-###############
-spam <- read.table("spambase/spambase.data",sep=",")
-#spam[,58] <- as.factor(spam[,58])
-
-resultsDF <- df.addrow(resultsDF,PCA.Compare(spam),"Spam")
-
-resultsDF
+  results_list <-
+    append(results_list, list(PCACompare(wine, dataset_name = "Wine")))
+}
 
 
-##################
-## Climate Data ##
-##################
+## Spam Data* ####
 
-clim <- read.table("pop_failures.dat", sep="", header=T)
-
-resultsDF <- df.addrow(resultsDF,PCA.Compare(clim[,3:21]),"Climate")
-
-
-################
-## Blood Data ##
-################
-
-blood <- read.table("transfusion.data", sep=",", header=T)
-
-resultsDF <- df.addrow(resultsDF,PCA.Compare(blood),"Blood")
+if (!BUILTIN_ONLY) {
+  spam <- read_csv(here("data/spambase.data"), col_names = FALSE) %>%
+    mutate(X58 = as.factor(X58))
+  
+  results_list <-
+    append(results_list, list(PCACompare(spam, dataset_name = "Spam")))
+}
 
 
-################
-## Liver Data ##
-################
+## Climate Data* ####
 
-liver <- read.table("Indian Liver Patient Dataset (ILPD).csv",sep=",")
-liver$V2 <- as.numeric(liver$V2)
-
-resultsDF <- df.addrow(resultsDF,PCA.Compare(liver),"Liver")
-
-
-##############
-## Fog Data ##
-##############
-
-fog <- read.table("dataset_fog_release/dataset/S01R01.txt")
-
-resultsDF <- df.addrow(resultsDF,PCA.Compare(fog,verbose=T),"FOG")
+if (!BUILTIN_ONLY) {
+  clim <- read_table(here("data/pop_failures.dat")) %>%
+    select(-Study,-Run) %>%
+    mutate(outcome = as.factor(outcome))
+  
+  results_list <-
+    append(results_list, list(PCACompare(clim, dataset_name = "Climate")))
+}
 
 
-##################
-## Seismic Data ##
-##################
+## Blood Data* ####
 
-seismic <- read.table("seismic-bumps.arff",sep=",",skip=154)
-
-resultsDF <- df.addrow(resultsDF,PCA.Compare(seismic[,c(4:7,9:13,17:19)]),"Seismic")
-
-
-################
-## Glass Data ##
-################
-
-glass <- read.table("glass/glass.data",sep=",")
-
-resultsDF <- df.addrow(resultsDF,PCA.Compare(glass),"Glass")
+if (!BUILTIN_ONLY) {
+  blood <-
+    read_csv(here("data/transfusion.data"),
+             col_names = FALSE,
+             skip = 1) %>%
+    mutate(X5 = as.factor(X5))
+  
+  results_list <-
+    append(results_list, list(PCACompare(blood, dataset_name = "Blood")))
+}
 
 
+## Liver Data* ####
 
-########################
-## Breast Cancer Data ##
-########################
-
-data(BreastCancer)
-
-resultsDF <- df.addrow(resultsDF,PCA.Compare(BreastCancer[,-1]),"Cancer")
-
-
-################
-## Sonar Data ##
-################
-
-data(Sonar)
-
-resultsDF <- df.addrow(resultsDF,PCA.Compare(Sonar),"Sonar")
-
-###################
-## Diabetes Data ##
-###################
-
-data(PimaIndiansDiabetes)
-
-resultsDF <- df.addrow(resultsDF,PCA.Compare(PimaIndiansDiabetes),"Diabetes")
-
-#####################
-## Ionosphere Data ##
-#####################
-
-data(Ionosphere)
-
-resultsDF <- df.addrow(resultsDF,PCA.Compare(Ionosphere[,3:35]),"Ionosphere")
+if (!BUILTIN_ONLY) {
+  liver <-
+    read_csv(here("data/Indian Liver Patient Dataset (ILPD).csv"),
+             col_names = FALSE) %>%
+    mutate(X2 = ifelse(X2 == "Female", 1, 0),
+           X11 = as.factor(X11)) %>%
+    filter(!is.na(X10)) # Removes 4 rows with missing value
+  
+  results_list <-
+    append(results_list, list(PCACompare(liver, dataset_name = "Liver")))
+}
 
 
-###################
-## Vehicles Data ##
-###################
-vehicles <- read.table("vehicles/vehicles.dat")
+## Fog Data* ####
 
-resultsDF <- df.addrow(resultsDF,PCA.Compare(vehicles),"Vehicles")
+if (!BUILTIN_ONLY) {
+  fog <- read_table(here("data/S01R01.txt"), col_names = FALSE) %>%
+    mutate(X11 = as.factor(X11))
 
-
-################
-## Yeast Data ##
-################
-
-yeast <- read.table("yeast/yeast.data")
-##Removed columns with low variance
-resultsDF <- df.addrow(resultsDF,PCA.Compare(yeast[,c(-1,-6,-7)]),"Yeast")
+  results_list <-
+    append(results_list, list(PCACompare(fog, dataset_name = "FOG")))
+}
 
 
+## Seismic Data* ####
 
-##                  Orig       PCA NFeatures NGroups   NObs        Change
-## Hepta       1.0000000 1.0000000         3       7    212  0.0000000000
-## Lsun        1.0000000 0.9975000         2       3    400 -0.0025000000
-## Tetra       1.0000000 1.0000000         3       4    400  0.0000000000
-## Chainlink   0.9798000 0.6538000         3       2   1000 -0.3260000000
-## Atom        0.9953750 0.9952500         3       2    800 -0.0001250000
-## EngyTime    0.9574463 0.9566406         2       2   4096 -0.0008056641
-## Target      1.0000000 1.0000000         2       6    770  0.0000000000
-## TwoDiamonds 1.0000000 1.0000000         2       2    800  0.0000000000
-## WingNut     0.9917323 0.9646654         2       2   1016 -0.0270669291
-## GolfBall           NA        NA         3       1   4002            NA
-## Crabs       0.3740000 0.9330000         5       4    200  0.5590000000
-## Iris        0.9533333 0.9206667         4       3    150 -0.0326666667
-## Wine        0.9730337 0.9573034        13       3    178 -0.0157303371
-## Spam        0.7131928 0.7484677        57       2   4601  0.0352749402
-## Climate     0.9490741 0.9333333        18       2    540 -0.0157407407
-## Blood       0.7505348 0.7783422         4       2    748  0.0278074866
-## Liver       0.5507719        NA        10       2    583            NA
-## FOG         0.7806582 0.8546659        10       3 151987  0.0740076454
-## Seismic            NA 0.8885836        14       2   2584            NA
-## Glass              NA 0.9355140        10       6    214            NA
-## Cancer      0.9738197        NA         9       2    699            NA
-## Sonar       0.6793269 0.7250000        60       2    208  0.0456730769
-## Diabetes    0.7541667 0.7467448         8       2    768 -0.0074218750
-## Ionosphere  0.8190883 0.9031339        32       2    351  0.0840455840
-## Vehicles    0.4576832 0.7621749        18       4    846  0.3044917
-## Yeast       0.5715633 0.552628          6      10   1484 -0.0189353
+if (!BUILTIN_ONLY) {
+  seismic <- read_csv(here("data/seismic-bumps.arff"), col_names=FALSE, skip = 154) %>%
+    select(X4:X7, X9:X13, X17:X19) %>%
+    mutate(X19 = as.factor(X19))
+  
+  results_list <-
+    append(results_list, list(PCACompare(seismic, dataset_name = "Seismic")))
+}
 
+
+## Glass Data ####
+
+data(Glass, package="mlbench")
+
+glass <- mutate(Glass, Type = as.factor(Type))
+  
+results_list <-
+  append(results_list, list(PCACompare(glass, dataset_name = "Glass")))
+
+
+## Breast Cancer Data ####
+
+data(biopsy, package="MASS")
+
+cancer = biopsy %>% 
+  select(-ID) %>%
+  filter(!is.na(V6)) # Removes 16 rows
+
+results_list <-
+  append(results_list, list(PCACompare(cancer, dataset_name = "Cancer")))
+
+
+## Sonar Data #####
+
+data(Sonar, package="mlbench")
+
+
+results_list <-
+  append(results_list, list(PCACompare(Sonar, dataset_name = "Sonar")))
+
+
+## Diabetes Data #####################
+
+data(PimaIndiansDiabetes, package="mlbench")
+
+results_list <-
+  append(results_list, list(PCACompare(PimaIndiansDiabetes, dataset_name = "Diabetes")))
+
+
+## Ionosphere Data #######################
+
+data(Ionosphere, package="mlbench")
+
+iono <- Ionosphere %>%
+  select(V3:Class)
+
+results_list <-
+  append(results_list, list(PCACompare(iono, dataset_name = "Ionosphere")))
+
+
+## Vehicles Data #####################
+
+data(Vehicle, package="mlbench")
+
+results_list <-
+  append(results_list, list(PCACompare(Vehicle, dataset_name = "Vehicle")))
+
+
+## Yeast Data* ##################
+
+if (!BUILTIN_ONLY) {
+  yeast <- read_table(here("data/yeast.data"), col_names=FALSE) %>%
+    select(-X1, -X6, -X7) %>%
+    mutate(X10 = as.factor(X10))
+  
+  results_list <-
+    append(results_list, list(PCACompare(yeast, dataset_name = "Yeast")))
+}
+
+## Displaying and Saving Summary ####
+
+results_df = bind_rows(results_list)
+write_csv(results_df, here("data/results_table.csv"))
+
+print(results_df, n=nrow(results_df))
